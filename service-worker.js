83f1ff2da4390645595f0c1415c24a4b9928b438
hhiruko <hhiruko@users.noreply.github.com>
2025-08-25 04:18:08 +0000

    deploy: cb74e44f1b83b5549a44b64e08f7514f7b3e077f

--- service-worker.js
+++ service-worker.js
@@ -74,11 +74,11 @@
   "/custom-workout-logger/cdn/water.system.min.css",
   "/custom-workout-logger/cdn/water.light.min.css",
   "/custom-workout-logger/cdn/water.dark.min.css",
-  "/custom-workout-logger/assets/index-Eqc4L5LK.js",
   "/custom-workout-logger/assets/index-BoblkXAy.css",
+  "/custom-workout-logger/assets/index-BGT-sj5X.js",
   "/custom-workout-logger/"
 ];
-    const CACHE_NAME = 'v2025-08-22T13:07:32.369Z';
+    const CACHE_NAME = 'v2025-08-25T04:18:07.637Z';
 
     self.addEventListener('install', event => {
         self.skipWaiting();
